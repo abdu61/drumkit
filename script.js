3b0ab9756fa8ab1aabb752a71d00528df97894e2
abdu61 <112977047+abdu61@users.noreply.github.com>
2023-12-02 21:15:20 +0300

    Edit Main Functionality and Added More Styling

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ function playSound(e) {
   const keys = Array.from(document.querySelectorAll('.key'));
   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
   window.addEventListener('keydown', playSound);
+
+  //Background animation
+// Function to create a span with random properties
+function createSpan() {
+    const span = document.createElement('span');
+    const size = Math.random() * 15;
+
+    span.style.width = size + 'px';
+    span.style.height = size + 'px';
+    span.style.borderRadius = `80%`;
+    span.style.top = Math.random() * innerHeight + "px";
+    span.style.left = Math.random() * innerWidth + "px";
+    span.style.transition = 'all 0.7s ease-out'; 
+    span.style.background = 'white';
+
+    return span;
+}
+
+// Main animation function
+function backgroundAnimation() {
+    const section = document.querySelector('main');
+    const span = createSpan();
+
+    section.appendChild(span);
+    setTimeout(() => {span.remove()}, Math.random() * 4000 + 3000);
+}
+
+setInterval(backgroundAnimation, 150);
